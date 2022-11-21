511c15d5337e82903ab69d7dc899a205456a27e0
Arrturo <artur.jeznach.8@gmail.com>
2022-11-21 12:24:01 +0100

    update main

--- Django_Project/Bookstore/models.py
+++ Django_Project/Bookstore/models.py
@@ -3,61 +3,47 @@ import datetime
 from django.core.exceptions import ValidationError
 from django.utils.translation import gettext_lazy as _
 
-# Validation Functions
-
-
-def Date_validation(date: datetime):
+#Validation Functions
+def Date_validation(date:datetime):
     if date > datetime.date.today():
-        raise ValidationError(
-            _("Data nie może być nowsza niż dzisiejsza data."))
-
+        raise ValidationError(_("Data nie może być nowsza niż dzisiejsza data."))
 
 def Price_validation(price):
     if price < 0:
         raise ValidationError(_("Cena nie może być wartością ujemną."))
 
     if round(price, 2) != price:
-        raise ValidationError(
-            _("Cena może być sprecyzowana do jednego grosza."))
+        raise ValidationError(_("Cena może być sprecyzowana do jednego grosza."))
 
-
-def Name_validation(name: str):
+def Name_validation(name:str):
     x = name.replace(" ", "")
     if not x.isalpha():
         raise ValidationError(_("Dane mogą zwierać tylko litery."))
 
-# Models
-
-
+#Models
 class Book(models.Model):
     book_id = models.AutoField(primary_key=True)
     title = models.CharField(max_length=100)
-    author = models.ForeignKey(
-        'Author', on_delete=models.SET_NULL, null=True, validators=[Name_validation])
+    author = models.ForeignKey('Author', on_delete=models.SET_NULL, null=True, validators=[Name_validation])
     price = models.FloatField(validators=[Price_validation])
     description = models.TextField()
     publisher = models.CharField(max_length=45)
     number_of_copies = models.IntegerField()
     year_of_publication = models.DateField(validators=[Date_validation])
-    genre_genre = models.ForeignKey(
-        'Genre', on_delete=models.SET_NULL, null=True,  validators=[Name_validation])
-    section_section = models.ForeignKey(
-        'Section', on_delete=models.SET_NULL, null=True,  validators=[Name_validation])
-
+    genre_genre = models.ForeignKey('Genre', on_delete=models.SET_NULL, null=True,  validators=[Name_validation])
+    section_section = models.ForeignKey('Section', on_delete=models.SET_NULL, null=True,  validators=[Name_validation])
+    
     def __str__(self):
         return self.title
 
-
 class Order(models.Model):
     order_id = models.AutoField(primary_key=True)
-    client_client = models.ForeignKey(
-        'Client', on_delete=models.SET_NULL, null=True)
+    client_client = models.ForeignKey('Client', on_delete=models.SET_NULL, null=True)
     book_book = models.ForeignKey('Book', on_delete=models.SET_NULL, null=True)
     purchase_date = models.DateField(validators=[Date_validation])
 
     def __str__(self):
         return self.client_client.title
-
 
 class Author(models.Model):
     author_id = models.AutoField(primary_key=True)
@@ -66,7 +52,6 @@ class Author(models.Model):
 
     def __str__(self):
         return self.name + ' ' + self.last_name
-
 
 class Client(models.Model):
     client_id = models.AutoField(primary_key=True)
@@ -79,7 +64,6 @@ class Client(models.Model):
     def __str__(self):
         return self.name
 
-
 class Section(models.Model):
     section_id = models.AutoField(primary_key=True)
     section_name = models.CharField(max_length=45)
@@ -87,10 +71,11 @@ class Section(models.Model):
     def __str__(self):
         return self.section_name
 
-
 class Genre(models.Model):
     genre_id = models.AutoField(primary_key=True)
     genre = models.CharField(max_length=50, validators=[Name_validation])
-
     def __str__(self):
+        return self.genre
+    
+    def __str__(self) -> str:
         return self.genre
